342278fb006fd8252c2c4c8f58800eb733bf1850
Alison Ortiz <alison.og@hotmail.com>
2022-05-05 13:43:45 -0500

    Initial commit

---
+++ src/test/java/com/exampleali/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleali.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
